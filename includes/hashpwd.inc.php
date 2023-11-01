d26b887f39eec54b3ce47460dd9bc18f39c4297c
jankorczak <jasiukorczak@gmail.com>
2023-11-01 15:53:31 +0100

    Tutorial done, now making a project

--- includes/hashpwd.inc.php
+++ includes/hashpwd.inc.php
@@ -1,3 +1,17 @@
 <?php
 
- password_hash();
+$pwdSignup = "Krossing";
+
+$options = [
+    'cost' => 12
+];
+
+ $hashedPwd = password_hash($pwdSignup, PASSWORD_BCRYPT, $options);
+
+ $pwdLogin = "Krossing";
+
+ if (password_verify($pwdLogin, $hashedPwd)) {
+    echo "They are the same!";
+ } else {
+    echo "They are not the same!";
+ }
